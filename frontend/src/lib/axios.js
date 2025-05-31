e8ecdab32db93dc91dff8703d8cce1580825a16e
Saud0786 <saud31253@gmail.com>
2025-05-31 13:07:56 +0530

    Backend Url added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance =axios.create({
-  baseURL:'http://localhost:5002/api',
+  baseURL:'https://chatapplication-backend-i6ae.onrender.com/api',
   withCredentials: true
 })
